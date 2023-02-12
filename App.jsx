9bbb3e5e65c7859ef7ff2e6254d090bd9dd08fd5
benNjogu <kamandemachafi@gmail.com>
2023-02-12 17:37:46 +0300

    assignment2

--- App.jsx
+++ App.jsx
@@ -5,13 +5,33 @@ const App = () => {
   return (
     <View style={styles.body}>
       <View style={styles.view1}>
-        <Text style={styles.text}>1</Text>
+        <View style={styles.innerView1}>
+          <Text style={styles.text}>1</Text>
+        </View>
+        <View style={styles.innerView2}>
+          <Text style={styles.text}>2</Text>
+        </View>
+        <View style={styles.innerView3}>
+          <Text style={styles.text}>3</Text>
+        </View>
       </View>
+
       <View style={styles.view2}>
-        <Text style={styles.text}>2</Text>
+        <Text style={styles.text}>4</Text>
       </View>
+
       <View style={styles.view3}>
-        <Text style={styles.text}>3</Text>
+        <Text style={styles.text}>5</Text>
+      </View>
+
+      <View style={styles.view4}>
+        <View style={styles.innerView6}>
+          <Text style={styles.text}>6</Text>
+        </View>
+
+        <View style={styles.innerView7}>
+          <Text style={styles.text}>7</Text>
+        </View>
       </View>
     </View>
   );
@@ -20,10 +40,10 @@ const App = () => {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
     backgroundColor: '#0000ff',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
+    alignItems: 'stretch',
+    justifyContent: 'center',
   },
   text: {
     color: '#fff',
@@ -31,26 +51,54 @@ const styles = StyleSheet.create({
     fontStyle: 'italic',
     margin: 10,
   },
-  view1: {
-    width: 100,
-    height: 100,
-    backgroundColor: '#00ffff',
+  innerView1: {
+    flex: 1,
+    backgroundColor: '#0fffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  innerView2: {
+    flex: 2,
+    backgroundColor: '#000fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  innerView3: {
+    flex: 3,
+    backgroundColor: '#ff000f',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  innerView6: {
+    flex: 1,
+    backgroundColor: '#789546',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  innerView7: {
+    flex: 1,
+    backgroundColor: '#897457',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view1: {
+    backgroundColor: '#00ffff',
+    flexDirection: 'row',
+  },
   view2: {
-    width: 100,
-    height: 100,
     backgroundColor: '#ff00ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   view3: {
-    width: 100,
-    height: 100,
-    backgroundColor: '#ffff00',
+    backgroundColor: '#ff6700',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  view4: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#00ff00',
   },
 });
 
