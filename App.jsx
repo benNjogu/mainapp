d9b1a45c697a1753edf4954e2f8b23ae5221d04b
benNjogu <kamandemachafi@gmail.com>
2023-02-13 12:55:38 +0300

    section list

--- App.jsx
+++ App.jsx
@@ -6,6 +6,7 @@ import {
   ScrollView,
   RefreshControl,
   FlatList,
+  SectionList,
 } from 'react-native';
 
 const App = () => {
@@ -21,6 +22,25 @@ const App = () => {
   ]);
   const [refreshing, setRefreshing] = useState(false);
 
+  const DATA = [
+    {
+      title: 'Title 1',
+      data: ['item 1-1', 'item 1-2', 'item 1-3'],
+    },
+    {
+      title: 'Title 2',
+      data: ['item 2-1', 'item 2-2', 'item 2-3'],
+    },
+    {
+      title: 'Title 3',
+      data: ['item 3-1', 'item 3-2', 'item 3-3'],
+    },
+    {
+      title: 'Title 4',
+      data: ['item 4-1', 'item 4-2', 'item 4-3'],
+    },
+  ];
+
   const onRefresh = () => {
     setRefreshing(true);
     setItems([...items, {name: 'Item 79'}]);
@@ -28,22 +48,32 @@ const App = () => {
   };
 
   return (
-    <FlatList
-      keyExtractor={(item, index) => index.toString()}
-      data={items}
-      renderItem={({item}) => (
+    <SectionList
+      sections={DATA}
+      renderItem={({item}) => <Text style={styles.text}>{item}</Text>}
+      renderSectionHeader={({section}) => (
         <View style={styles.item}>
-          <Text style={styles.text}>{item.name}</Text>
+          <Text style={styles.text}>{section.title}</Text>
         </View>
       )}
-      refreshControl={
-        <RefreshControl
-          refreshing={refreshing}
-          onRefresh={onRefresh}
-          colors={['#ff00ff']}
-        />
-      }
     />
+
+    // <FlatList
+    //   keyExtractor={(item, index) => index.toString()}
+    //   data={items}
+    //   renderItem={({item}) => (
+    //     <View style={styles.item}>
+    //       <Text style={styles.text}>{item.name}</Text>
+    //     </View>
+    //   )}
+    //   refreshControl={
+    //     <RefreshControl
+    //       refreshing={refreshing}
+    //       onRefresh={onRefresh}
+    //       colors={['#ff00ff']}
+    //     />
+    //   }
+    // />
 
     // <ScrollView
     //   style={styles.body}
