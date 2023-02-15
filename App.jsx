a3f59dbf22dc805647ec49e9301454b4b7212dde
benNjogu <kamandemachafi@gmail.com>
2023-02-15 14:54:52 +0300

    Navigation componet

--- App.jsx
+++ App.jsx
@@ -1,154 +1,74 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import React, {useState} from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  Pressable,
-  Alert,
-  ToastAndroid,
-  Modal,
-} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+
 import CustomButton from './components/CustomButton';
-import Header from './components/Header';
 
-const App = () => {
-  const [name, setName] = useState('');
-  const [submitted, setSubmitted] = useState(false);
-  const [showWarning, setShowWarning] = useState(false);
+const stack = createStackNavigator();
 
+const ScreenA = ({navigation}) => {
   const handlePress = () => {
-    if (name.length > 3) setSubmitted(!submitted);
-    else setShowWarning(true);
+    navigation.navigate('Screen_B');
   };
 
   return (
     <View style={styles.body}>
-      <Header />
-      <Modal
-        visible={showWarning}
-        transparent
-        onRequestClose={() => setShowWarning(false)}
-        animationType="slide"
-        hardwareAccelerated>
-        <View style={styles.centered_view}>
-          <View style={styles.warning_modal}>
-            <View style={styles.warning_title}>
-              <Text style={styles.text}>WARNING!</Text>
-            </View>
-            <View style={styles.warning_body}>
-              <Text style={styles.text}>
-                The name must be longer than 3 characters.
-              </Text>
-            </View>
-            <Pressable
-              onPress={() => setShowWarning(false)}
-              style={styles.warning_button}
-              android_ripple={{color: '#00000099'}}>
-              <Text style={styles.text}>OK</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-      <Text style={styles.text}>Please write your name:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="e.g. Ben"
-        onChangeText={value => setName(value)}
-      />
+      <Text style={styles.text}>Screen A</Text>
       <CustomButton
         onPress={handlePress}
-        title={submitted ? 'Clear' : 'Submit'}
+        title={'Screen B'}
+        color={'#ff00ff'}
+      />
+    </View>
+  );
+};
+
+const ScreenB = ({navigation}) => {
+  const handlePress = () => {
+    // navigation.navigate('Screen_A');
+    navigation.goBack();
+  };
+
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>Screen B</Text>
+      <CustomButton
+        onPress={handlePress}
+        title={'Screen A'}
         color={'#00ff00'}
       />
-      <CustomButton
-        onPress={handlePress}
-        title={submitted ? 'Tested' : 'Test'}
-        color={'#ff00ff'}
-        style={{margin: 10}}
-      />
-      {/* <Pressable
-        onPress={handlePress}
-        hitSlop={{top: 10, left: 10, bottom: 10, right: 10}}
-        android_ripple={{color: '#00f'}}
-        style={({pressed}) => [
-          {
-            backgroundColor: pressed ? '#dddddd' : '#00ff00',
-          },
-          styles.button,
-        ]}>
-        <Text style={styles.text}>{submitted ? 'Clear' : 'Submit'}</Text>
-      </Pressable> */}
-      {submitted && (
-        <Text style={styles.text}>Your are registered as: {name}</Text>
-      )}
     </View>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <stack.Navigator
+      //screenOptions={{header: () => null}}
+      >
+        <stack.Screen
+          name="Screen_A"
+          component={ScreenA}
+          //options={{header: () => null}}
+        />
+        <stack.Screen name="Screen_B" component={ScreenB} />
+      </stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-  },
-  button: {
-    width: 150,
-    height: 50,
-    alignItems: 'center',
-  },
-  centered_view: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#00000099',
-  },
-  input: {
-    width: 200,
-    borderWidth: 1,
-    borderColor: '#555',
-    borderRadius: 5,
-    textAlign: 'center',
-    fontSize: 16,
-    margin: 10,
   },
   text: {
-    color: '#000',
-    fontSize: 18,
+    fontSize: 40,
+    fontWeight: 'bold',
     margin: 10,
-    textAlign: 'center',
-  },
-  warning_body: {
-    height: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  warning_button: {
-    backgroundColor: '#fff',
-    borderWidth: 1,
-    borderTopColor: '#00000099',
-    borderBottomColor: '#fff',
-    borderLeftColor: '#fff',
-    borderRightColor: '#fff',
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
-  },
-  warning_modal: {
-    width: 300,
-    height: 300,
-    backgroundColor: '#fff',
-    borderWidth: 1,
-    borderColor: '#000',
-    borderRadius: 20,
-  },
-  warning_title: {
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ff0',
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
   },
 });
 
