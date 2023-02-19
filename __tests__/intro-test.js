ab39ba0a315a0696dea1d421d01f8a07cec70e7a
benNjogu <kamandemachafi@gmail.com>
2023-02-19 18:07:20 +0300

    Testing react native projects

---
+++ __tests__/intro-test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {FlatList, Text} from 'react-native';
+import renderer from 'react-test-renderer';
+import Intro from '../src/utils/intro';
+
+test('renders correctly', () => {
+  const tree = renderer.create(<Intro />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders the flatlist component', () => {
+  const tree = renderer
+    .create(
+      <FlatList
+        data={['Item1', 'Item2', 'Item3']}
+        keyExtractor={item => item}
+        renderItem={({item}) => <Text>{item}</Text>}
+      />,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
